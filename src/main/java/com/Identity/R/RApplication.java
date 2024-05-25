ff4c59acdb7ab239c644d93fd820089cbd084150
vikas-kumar-09 <vikashgupta1234x@gmail.com>
2024-05-25 10:15:46 +0530

    update main file

--- src/main/java/com/Identity/R/RApplication.java
+++ src/main/java/com/Identity/R/RApplication.java
@@ -8,6 +8,8 @@ public class RApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RApplication.class, args);
+
+		System.out.println("SpringBoot Started...");
 	}
 
 }
